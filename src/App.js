1993ce593a10279e6903fe9b812a8a87a707f9b8
lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2024-04-23 21:30:05 +0100

    Fin tech
    Web app head and logo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
-import './App.css'; 
+import './App.css';
+import imageFile from './assets/img/logo.png';
 
 function App() {
   return (
     <div className="App">
-    {/* your html goes here */}
+      <header id="header" class="header fixed-top d-flex align-items-center">
+      <div class="d-flex align-items-center justify-content-between">
+      <a href="App.js" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="" ></img>
+        <span class="d-none d-lg-block">FinTech</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div> 
+
+
+      </header>
     </div>
   );
 }
